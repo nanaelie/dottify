5601e353436d8cd60644c4823163fc4e8d5255ce
nanaelie <elienana92@gmail.com>
2025-05-20 20:34:20 +0000

    chore: updated module import and package metadata

--- dottify/__init__.py
+++ dottify/__init__.py
@@ -1,3 +1,24 @@
-from .dottify import Dottify
+"""
+                        dottify
 
-__all__ = ["Dottify"]
+    A lightweight utility to access dictionary keys
+    as attributes (dot notation), with case-insensitive 
+    matching and helpful error suggestions.
+
+    Author: nanaelie
+    Repository: https://github.com/nanaelie/dottify
+    License: MIT
+"""
+
+from .dottify import Dottify
+from .__version__ import __version__
+from .exceptions import *
+from .exceptions import __all__ as exceptions
+
+__url__ = "https://github.com/nanaelie/dottify"
+__author__ = "nanaelie"
+__license__ = "MIT"
+
+__all__ = exceptions + ['Dottify', '__version__', '__url__',
+                                '__author__', '__license__']
+
